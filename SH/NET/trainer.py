66ae347fd3e239b84a090c64e1cb40da3a655092
suryaprakash-G <suryagopinathan@gmail.com>
2019-10-28 19:20:46 +0530

    chatbot trainer in prrogress

--- SH/NET/trainer.py
+++ SH/NET/trainer.py
@@ -10,8 +10,26 @@ while true:
     x = input()
     sep = x.split(" ")
     print("type# ")
-
+    t = input()
 def model():
+    with open('intents.json') as json_data:
+        intents = json.load(json_data)
+    words = []
+    classes = []
+    documents = []
+    ignore_words = ['?']
+    # loop through each sentence in our intents patterns
+    for intent in intents['intents']:
+        for pattern in intent['patterns']:
+            # tokenize each word in the sentence
+            w = nltk.word_tokenize(pattern)
+            # add to our words list
+            words.extend(w)
+            # add to documents in our corpus
+            documents.append((w, intent['tag']))
+            # add to our classes list
+            if intent['tag'] not in classes:
+                classes.append(intent['tag'])
     tf.reset_default_graph()
     # Build neural network
     net = tflearn.input_data(shape=[None, len(train_x[-1])])
