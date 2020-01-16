9f772d781d7dfe9c45e487f2c409637e9fe6e8a6
surya <suryagopinathan@gmail.com>
2020-01-16 22:55:19 +0530

    just file writing enough neural net pending

--- SH/NET/COM/trainer.py
+++ SH/NET/COM/trainer.py
@@ -1,7 +1,5 @@
 
 import numpy as np
-#import tflearn
-import tensorflow as tf
 import random
 import fileinput
 import json
@@ -21,29 +19,3 @@ def main():
         fo.write("t-"+t+"\np-"+p+"\nl-"+l);
     fo.close();
 main()
-def model():
-    with open('intents.json') as json_data:
-        intents = json.load(json_data)
-    words = []
-    classes = []
-    documents = []
-    ignore_words = ['?']
-    # loop through each sentence in our intents patterns
-    for intent in intents['intents']:
-        for pattern in intent['patterns']:
-            # tokenize each word in the sentence
-            w = nltk.word_tokenize(pattern)
-            # add to our words list
-            words.extend(w)
-            # add to documents in our corpus
-            documents.append((w, intent['tag']))
-            # add to our classes list
-            if intent['tag'] not in classes:
-                classes.append(intent['tag'])
-    tf.reset_default_graph()
-    # Build neural network
-    net = tflearn.input_data(shape=[None, len(train_x[-1])])
-    net = tflearn.fully_connected(net, 7)
-    net = tflearn.fully_connected(net, 7)
-    net = tflearn.fully_connected(net, len(train_y[-1]), activation='softmax')
-    net = tflearn.regression(net)
